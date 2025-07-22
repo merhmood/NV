35dc7dc6f53832573e6baaca251eada173b4737d
Mahmud Suberu <mahmudsuberu9@gmail.com>
2025-07-22 23:26:45 +0100

    feat: add the subscribe page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,29 +3,40 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
 
 const Footer = () => {
   const path = usePathname();
-  const isTopUpPage = path === "/top-up";
   return (
     <footer className="border-t border-gray-600 text-white fixed bottom-0 w-full ">
-      <div className="flex w-4/12 mx-auto justify-between items-center py-2">
+      <div className="flex w-fit mx-auto items-center py-2 gap-4">
+        <Link href="/top-up">
+          <div
+            className={`${
+              path === "/top-up" && "bg-gray-500/20"
+            } rounded-full p-2`}
+          >
+            <div className="relative h-8 w-8 lg:h-14 lg:w-14 justify-center">
+              <Image src="/top-up.png" alt="top-up" fill />
+            </div>{" "}
+          </div>
+        </Link>
         <Link href="/">
           <div
-            className={`${!isTopUpPage && "bg-gray-500/20"} rounded-full p-2`}
+            className={`${path === "/" && "bg-gray-500/20"} rounded-full p-2`}
           >
             <div className="relative h-8 w-8 lg:h-14 lg:w-14 justify-center">
               <Image src="/home.png" alt="home" fill />
             </div>{" "}
           </div>
         </Link>
-        <Link href="/top-up">
+        <Link href="/subscribe">
           <div
-            className={`${isTopUpPage && "bg-gray-500/20"} rounded-full p-2`}
+            className={`${
+              path === "/subscribe" && "bg-gray-500/20"
+            } rounded-full p-2`}
           >
             <div className="relative h-8 w-8 lg:h-14 lg:w-14 justify-center">
-              <Image src="/top-up.png" alt="top-up" fill />
+              <Image src="/subscribe.png" alt="top-up" fill />
             </div>{" "}
           </div>
         </Link>
